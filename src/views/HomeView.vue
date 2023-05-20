4125aa238dda5171b7d4e26ee871bb5b6e5cd6fb
jundasukron <muhd.jundasyukron@student.uir.ac.id>
2023-05-20 19:27:22 +0700

    update link github

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+   <h2>Tentang Saya</h2>
+   <p>MUHD. JUNDA SYUKRON</p>
+   <p>213510063</p>
+   <p>Teknik Informatika</p>
+   <h3>Universitas Islam Riau</h3>
+  </main>
+</template>
+<style>
+  h2{
+    text-align : center;
+    font-size: 25px;
+    font-weight: both;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: whitesmoke;
+  }
+  h3{
+    text-align : center;
+    font-size: 15px;
+    font-weight: both;
+    margin: 20px 0;
+    text-transform: uppercase;
+    color: white;
+
+  }
+  p{
+    text-align: center;
+    font-size: 10px;
+    margin: auto;
+    color: white;
+  }
+</style>
